a3aa41612ed735171d3691447eefd3ad965cb54c
siguremon <siguremon@gmail.com>
2015-03-10 00:19:03 +0900

    add install resources for redmine

--- recipe.rb
+++ recipe.rb
@@ -1,2 +1,14 @@
 include_recipe 'rtn_rbenv::user'
 
+package "sqlite3"
+package "libsqlite3-dev"
+package "imagemagick libmagickwand-dev"
+
+execute "get redmine package" do 
+  command "wget http://www.redmine.org/releases/redmine-3.0.0.tar.gz ;" \
+    "tar xzf redmine-3.0.0.tar.gz"
+  not_if "test -e redmine-3.0.0.tar.gz"
+end
+
+
+
